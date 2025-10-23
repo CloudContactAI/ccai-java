4c4a94d88d946e6e1062195511b9c2a873bd9699
joel-maws <joel@mobileaws.com>
2025-10-23 16:03:01 -0700

    making it so the webserver doesn't start when you send SMS messages

--- example/src/main/java/com/example/javasdk/JavaSdkTestApplication.java
+++ example/src/main/java/com/example/javasdk/JavaSdkTestApplication.java
@@ -3,15 +3,12 @@ package com.example.javasdk;
 import com.cloudcontactai.sdk.CCAIClient;
 import com.cloudcontactai.sdk.common.CCAIConfig;
 import com.cloudcontactai.sdk.sms.SMSResponse;
-import com.cloudcontactai.sdk.sms.Account;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication
@@ -19,6 +16,10 @@ import java.util.Map;
 public class JavaSdkTestApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
+        if (args.length > 0 && "sms".equals(args[0])) {
+            runSmsTest();
+            return;
+        }
         SpringApplication.run(JavaSdkTestApplication.class, args);
     }
 
@@ -43,20 +44,12 @@ public class JavaSdkTestApplication implements CommandLineRunner {
         }
     }
 
-    /*
-     * Pass 'sms' to send SMS messages
-     * The default behavior is to start the webhook server on the port specified in application.properties
-     */
     @Override
     public void run(String... args) throws Exception {
-        if (args.length > 0 && "sms".equals(args[0])) {
-            runSmsTest();
-        } else {
-            System.out.println("Webhook server started. Use 'sms' argument to test SMS sending.");
-        }
+        System.out.println("Webhook server started. Use 'sms' argument to test SMS sending.");
     }
     
-    private void runSmsTest() throws Exception {
+    private static void runSmsTest() {
         System.out.println("Testing CCAI Java SDK...");
         
         CCAIConfig config = new CCAIConfig(
@@ -68,7 +61,7 @@ public class JavaSdkTestApplication implements CommandLineRunner {
         
         try {
             SMSResponse response = client.getSms().sendSingle(
-                "Johns", "Does", "+14158906431",
+                "John", "Doe", "+14158906431",
                 "Test SMS", "Test Campaign", "+13056486693"
             );
             System.out.println("SMS sent: " + response.getId());
