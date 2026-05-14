16fd6d3bd3f4e6274f18dbe87038ab84122c662a
Deyner lopez <dlopez@allcode.com>
2026-05-14 17:45:13 -0500

    https://mobileaws.atlassian.net/browse/CLOUD-2744

---
+++ src/main/kotlin/com/cloudcontactai/sdk/contactvalidator/ContactValidatorModels.kt
@@ -0,0 +1,38 @@
+package com.cloudcontactai.sdk.contactvalidator
+
+data class EmailValidationResult(
+    @com.fasterxml.jackson.annotation.JsonProperty("contact") val contact: String,
+    val type: String,
+    val status: String,
+    val metadata: Map<String, Any?> = emptyMap()
+)
+
+data class PhoneValidationResult(
+    @com.fasterxml.jackson.annotation.JsonProperty("contact") val contact: String,
+    val type: String,
+    val status: String,
+    val metadata: Map<String, Any?> = emptyMap()
+)
+
+data class ValidationSummary(
+    val total: Int,
+    val valid: Int,
+    val invalid: Int,
+    val risky: Int,
+    val landline: Int = 0
+)
+
+data class BulkEmailValidationResult(
+    val results: List<EmailValidationResult>,
+    val summary: ValidationSummary
+)
+
+data class BulkPhoneValidationResult(
+    val results: List<PhoneValidationResult>,
+    val summary: ValidationSummary
+)
+
+data class PhoneInput(
+    val phone: String,
+    val countryCode: String? = null
+)
