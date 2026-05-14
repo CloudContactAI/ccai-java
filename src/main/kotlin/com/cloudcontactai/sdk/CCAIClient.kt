53ceacae5712b268682e94bdf0a7a0ae205b06d0
Deyner lopez <dlopez@allcode.com>
2026-05-14 17:45:13 -0500

    https://mobileaws.atlassian.net/browse/CLOUD-2744

--- src/main/kotlin/com/cloudcontactai/sdk/CCAIClient.kt
+++ src/main/kotlin/com/cloudcontactai/sdk/CCAIClient.kt
@@ -3,6 +3,7 @@ package com.cloudcontactai.sdk
 import com.cloudcontactai.sdk.common.ApiClient
 import com.cloudcontactai.sdk.common.CCAIConfig
 import com.cloudcontactai.sdk.contact.ContactService
+import com.cloudcontactai.sdk.contactvalidator.ContactValidatorService
 import com.cloudcontactai.sdk.sms.SMSService
 import com.cloudcontactai.sdk.email.EmailService
 import com.cloudcontactai.sdk.webhook.WebhookService
@@ -12,7 +13,7 @@ import com.cloudcontactai.sdk.campaigns.CampaignService
 
 class CCAIClient(private val config: CCAIConfig) {
     private val apiClient = ApiClient(config)
-    
+
     val sms = SMSService(config, apiClient)
     val email = EmailService(config, apiClient)
     val webhook = WebhookService(config, apiClient)
@@ -20,7 +21,8 @@ class CCAIClient(private val config: CCAIConfig) {
     val contact = ContactService(config, apiClient)
     val brands = BrandService(config, apiClient)
     val campaigns = CampaignService(config, apiClient)
-    
+    val contactValidator = ContactValidatorService(config, apiClient)
+
     fun close() {
         // Cleanup resources if needed
     }
